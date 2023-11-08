28d799cea611ba8f510f57aa4422d16a1a35fc7b
maershaa <valeria.work2022@gmail.com>
2023-11-08 08:31:52 +0200

    base settings

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'; // npm install react-router-dom
+
 import { App } from 'components/App';
 import './index.css';
 
+//npm install axios
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App basename="/goit-react-hw-05-movies" />
+    </BrowserRouter>
   </React.StrictMode>
 );
